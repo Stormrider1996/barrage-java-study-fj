ce166af6c0ff598badec1eba3167b26096c6c010
Franjo Jumić <franjo.jumic@barrage.net>
2024-01-29 14:19:58 +0100

    add CRUD logic for TASK 2

--- src/main/java/com/setronica/eventing/persistence/Event.java
+++ src/main/java/com/setronica/eventing/persistence/Event.java
@@ -1,15 +1,29 @@
 package com.setronica.eventing.persistence;
 
-
 import java.time.LocalDate;
-import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "\"event\"")
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column
     private String title;
+    @Column
     private String description;
+    @Column
     private LocalDate date;
-    private List<String> images;
+    // leave it now for the next meetup session
+    // private List<String> images;
 
     public int getId() {
         return id;
@@ -41,13 +55,5 @@ public class Event {
 
     public void setDate(LocalDate date) {
         this.date = date;
-    }
-
-    public List<String> getImages() {
-        return images;
-    }
-
-    public void setImages(List<String> images) {
-        this.images = images;
     }
 }
